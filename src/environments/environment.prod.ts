e24b26be309f4c2e53c6e89495561737a6bc7230
Bineesh B <bineesh2107@gmail.com>
2017-10-08 19:35:24 +0530

    Firebase config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBCw5raMukOBHOfMtRAD8lYO3cCsL_udZw',
+    authDomain: 'quotes-i-like.firebaseapp.com',
+    databaseURL: 'https://quotes-i-like.firebaseio.com',
+    projectId: 'quotes-i-like',
+    storageBucket: 'quotes-i-like.appspot.com',
+    messagingSenderId: '210925766672'
+  }
 };
